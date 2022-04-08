8f41672b55d2ad4b21778d813188ddebe54f5e9b
root <adoniassahidoniassahilegithub@gmail.com>
2022-04-08 08:08:34 -0700

    task 11

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+int c, i, k;
+
+for (c = 0; c < 8; c++)
+{
+        for (i = c + 1 ; i < 9; i++)
+        {
+		for (k = i + 1 ; k < 10; k++)
+       		 {
+                putchar((c % 10) + '0');
+                putchar((i % 10) + '0');
+		putchar((k % 10) + '0');
+                if (c == 7 && i == 8 && k == 9)
+                        continue;
+                putchar(',');
+                putchar(' ');
+		}
+        }
+}
+putchar('\n');
+
+return (0);
+}

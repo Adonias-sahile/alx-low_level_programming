36fc5a16cad8b2a69974a3b6ac33f3e8c5c422de
root <adoniassahidoniassahilegithub@gmail.com>
2022-04-08 08:14:35 -0700

    task 11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -10,17 +10,17 @@ int c, i, k;
 
 for (c = 0; c < 8; c++)
 {
-        for (i = c + 1 ; i < 9; i++)
+	for (i = c + 1 ; i < 9; i++)
 	{
 		for (k = i + 1 ; k < 10; k++)
 		{
                 putchar((c % 10) + '0');
                 putchar((i % 10) + '0');
 		putchar((k % 10) + '0');
-                if (c == 7 && i == 8 && k == 9)
-                        continue;
-                putchar(',');
-                putchar(' ');
+		if (c == 7 && i == 8 && k == 9)
+			continue;
+		putchar(',');
+		putchar(' ');
 		 }
 	}
 }

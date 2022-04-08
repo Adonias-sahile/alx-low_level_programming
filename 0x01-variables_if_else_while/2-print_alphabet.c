2b1ef3740ce185e2e30dfdeeb10059256ef8540c
root <adoniassahidoniassahilegithub@gmail.com>
2022-04-08 05:06:18 -0700

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+int c;
+for (c = 0; c < 20; ++c)
+putchar('a' + c);
+
+putchar('\n');
+
+return (0);
+}
